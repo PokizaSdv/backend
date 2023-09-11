9da6c136bfa5f72a64c690bdb2d902956b293ef6
Pokiza Sdv <pokizasdv@gmail.com>
2023-09-11 00:46:05 -0400

    Did rest methods

--- assignments/assignment-1/solution.js
+++ assignments/assignment-1/solution.js
@@ -11,25 +11,30 @@
 // Calculate the monthly savings for each individual. Monthly savings are calculated by subtracting total expenses from total income for each month for each person.
 const fs = require("fs");
 class FinancialAnalytics {
-    static readJSONData(filename, dataCallback, writeCallback) {
-        fs.readFile(filename, "utf-8", (err, data) => {
-            if (err) throw err;
-            const content = JSON.parse(data);
-            const transformedData = dataCallback(content);
-
-            writeCallback("newData.json", transformedData);
-        });
-    }
     static writeFile(fileUrl, data) {
         fs.writeFile(fileUrl, JSON.stringify(data), (err) => {
             if (err) throw err;
         });
     }
 
+    static readJSONData(filename, dataCallback, writeCallback) {
+        fs.readFile(filename, "utf-8", (err, data) => {
+            if (err) throw err;
+            const parsedData = JSON.parse(data);
+            const transformedData = dataCallback(parsedData);
+
+            fs.writeFile(filename, JSON.stringify(transformedData), (err) => {
+                if (err) throw err;
+                writeCallback();
+            });
+        });
+    }
+
     static calculateMonthlySavings(data) {
         const monthlySavings = {};
-        for (const personId in content) {
-            const person = content[personId];
+        for (const personId in data) {
+            const person = data[personId];
+            monthlySavings[personId] = {};
             for (const month in person.expenses) {
                 const monthlyExpenses = person.expenses[month];
                 const monthlyIncomes = person.incomes[month];
@@ -39,11 +44,94 @@ class FinancialAnalytics {
 
                 const monthlySaving = monthlyIncomes - monthlyExpensesSum;
 
-                monthlySavings[month] = monthlySaving;
+                monthlySavings[personId][month] = monthlySaving;
             }
         }
         return monthlySavings;
     }
 
-    compareMonthlyExpenses() {}
+    static compareMonthlyExpenses(data) {
+        const monthlyExpensesComparison = {};
+
+        const categories = ["food", "rent", "entertainment", "transportation"];
+
+        for (const month in data.expenses) {
+            monthlyExpensesComparison[month] = {};
+            for (const category of categories) {
+                monthlyExpensesComparison[month][category] = {
+                    max: { id: null, amount: -Infinity },
+                    min: { id: null, amount: Infinity }
+                };
+            }
+            for (const personId in data) {
+                const person = data[personId];
+                for (const category of categories) {
+                    const expense = person.expenses[month][category];
+                    if (
+                        expense >
+                        monthlyExpensesComparison[month][category].max.amount
+                    ) {
+                        monthlyExpensesComparison[month][category].max.id =
+                            person.id;
+                        monthlyExpensesComparison[month][category].max.amount =
+                            expense;
+                    }
+                    if (
+                        expense <
+                        monthlyExpensesComparison[month][category].min.amount
+                    ) {
+                        monthlyExpensesComparison[month][category].min.id =
+                            person.id;
+                        monthlyExpensesComparison[month][category].min.amount =
+                            expense;
+                    }
+                }
+            }
+        }
+        return monthlyExpensesComparison;
+    }
+
+    static calculateAnnualExpenses(data) {
+        const annualExpenses = {};
+        for (const personId in data) {
+            const person = data[personId];
+            annualExpenses[personId] = {};
+
+            for (const month in person.expenses) {
+                const monthlyExpenses = person.expenses[month];
+
+                for (const category in monthlyExpenses) {
+                    annualExpenses[personId][category] +=
+                        monthlyExpenses[category];
+                }
+            }
+        }
+        return annualExpenses;
+    }
 }
+
+// const dataFilename = "data.json";
+
+FinancialAnalytics.readJSONData(
+    "data.json",
+    (data) => {
+        // Method 2: Calculate monthly savings
+        const monthlySavingsData =
+            FinancialAnalytics.calculateMonthlySavings(data);
+
+        // Method 3: Compare monthly expenses
+        const monthlyExpensesComparisonData =
+            FinancialAnalytics.compareMonthlyExpenses(data);
+
+        // Method 4: Calculate annual expenses
+        const annualExpensesData =
+            FinancialAnalytics.calculateAnnualExpenses(data);
+
+        console.log(monthlySavingsData);
+        console.log(monthlyExpensesComparisonData);
+        console.log(annualExpensesData);
+    },
+    () => {
+        console.log("Data transformation and writing completed.");
+    }
+);
