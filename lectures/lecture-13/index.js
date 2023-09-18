6ace4f6aae97d621838b3c0dc463acf9021ef732
Pokiza Sdv <pokizasdv@gmail.com>
2023-09-18 00:00:23 -0400

    Add Some Changes

--- lectures/lecture-13/index.js
+++ lectures/lecture-13/index.js
@@ -16,8 +16,12 @@ const parseId = (path) => {
     const parts = path.split("/");
     return parts[2];
 };
+const verifyPathMatch = (path) => {
+    return path.split("/").lenth === 3;
+};
 
 const server = http.createServer((req, res) => {
+    const isPathMatch = verifyPathMatch(req.url);
     if (req.method === "GET" && req.url === "/todos") {
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify(Object.values(todos)));
@@ -39,7 +43,7 @@ const server = http.createServer((req, res) => {
             res.writeHead(201, { "Content-Type": "application/json" });
             res.end(JSON.stringify(newTodo));
         });
-    } else if (req.method === "PATCH" && req.url.split("/").length === 3) {
+    } else if (req.method === "PATCH" && isPathMatch) {
         let body = [];
         req.on("data", (chunk) => {
             body.push(chunk);
@@ -52,12 +56,12 @@ const server = http.createServer((req, res) => {
             res.statusCode = 204;
             res.end();
         });
-    } else if (req.method === "GET" && req.url.split("/").length === 3) {
+    } else if (req.method === "GET" && isPathMatch) {
         const id = parseId(req.url);
         const todo = todos[id];
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify(todo));
-    } else if (req.method === "DELETE" && req.url.split("/").length === 3) {
+    } else if (req.method === "DELETE" && isPathMatch) {
         const id = parseId(req.url);
         delete todos[id];
         res.statusCode = 200;
