fe6f08878a063322182b0a821b596db23fe8ea58
Pokiza Sdv <pokizasdv@gmail.com>
2023-09-25 06:05:39 -0400

    Did Some Parts of Assignment about income-expense app with promises

---
+++ assignments/assignment-7/routes/income.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { incomesController } from "../controllers/income.controller.js";
+// import { validationMiddleware } from "../middlewares/middleware.js";
+
+const incomesRouter = new Router();
+
+incomesRouter.get("/", incomesController.getAllIncomes);
+incomesRouter.get("/:incomeId", incomesController.getIncomeById);
+incomesRouter.post("/", incomesController.createIncome);
+
+export { incomesRouter };
