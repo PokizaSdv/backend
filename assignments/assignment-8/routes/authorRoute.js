d1e23f96eb007c5a2ddb2f1fde82abcb9567f99f
Pokiza Sdv <pokizasdv@gmail.com>
2023-09-26 12:43:02 -0400

    Did Assignment-8 Part 1

---
+++ assignments/assignment-8/routes/authorRoute.js
@@ -0,0 +1,12 @@
+// import { Router } from "express";
+// import { authorController } from "../controllers/authorController.js";
+
+// const authorRouter = new Router();
+
+// authorRouter.get("/");
+// authorRouter.get("/:authorId");
+// authorRouter.post("/");
+// authorRouter.put("/:authorId");
+// authorRouter.delete("/:authorId");
+
+// export { authorRouter };
